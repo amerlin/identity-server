1261cb08f54efe35978194bda9e3ee8ec6613c8d
Andrea Merlin <a.merlin@keantex.com>
2020-04-12 22:05:58 +0200

    first commit

--- IdentityServerDemo/Startup.cs
+++ IdentityServerDemo/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServerDemo.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,13 @@ namespace IdentityServerDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //ADD IDENTITY SERVER
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+                .AddTestUsers(InMemoryConfig.GetUsers())
+                .AddInMemoryClients(InMemoryConfig.GetClients())
+                .AddDeveloperSigningCredential();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +34,18 @@ namespace IdentityServerDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            //ADD IDENTITY SERVER
+            app.UseIdentityServer();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
